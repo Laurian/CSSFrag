366f0d6e4ad3d37b19663c4e213f4281e9574e0f
Laurian Gridinoc <laurian@NERV.local>
2011-07-24 23:21:30 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+CSSFrag
+
+Fork of Shaun Inman 'CSS Selectors as Fragment Identifiers' implementation:
+https://twitter.com/shauninman/status/95230239906598913 
+
+Test it with the example link from https://twitter.com/shauninman/status/95230347649888256
---
+++ data/end.js
@@ -0,0 +1,27 @@
+function offsetXY(elem)
+{
+    var x = y = 0;
+	if (elem.offsetParent)
+	{
+		do {
+			x += elem.offsetLeft;
+			y += elem.offsetTop;
+		} while (elem = elem.offsetParent);
+	};
+	return {'x':x,'y':y};
+};
+
+// this page-mod runs at DOM ready
+//window.addEventListener('load', function()
+//{
+	var m;
+	if (m = decodeURIComponent(window.location.hash).match(/css\((.+)\)/))
+	{
+		var selector = m[1];
+		var elem = document.querySelector(selector);
+		var offset = offsetXY(elem);
+		window.scrollTo(offset.x, offset.y);
+		elem.focus();
+	};
+	
+//}, false);
